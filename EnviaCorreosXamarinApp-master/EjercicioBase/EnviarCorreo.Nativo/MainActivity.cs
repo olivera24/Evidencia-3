e4ac5612b5aebf97c17869564b3a4e76d10ea6c4
olivera24 <dashatoledo78@gmail.com>
2017-05-26 20:31:02 -0500

    subiendo archivor

---
+++ EnviaCorreosXamarinApp-master/EjercicioBase/EnviarCorreo.Nativo/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using EnviarCorreo.Nativo.Services;
+
+
+namespace EnviarCorreo.Nativo
+{
+    [Activity(Label = "EnviarCorreo.Nativo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Button button;
+    
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            button = FindViewById<Button>(Resource.Id.btnReportar);
+            button.Click += btnReportar_Click;
+        }
+
+        private async void btnReportar_Click(object sender, EventArgs e)
+        {
+            ServiceHelper serviceHelper = new ServiceHelper();
+
+            await serviceHelper.InsertarEntidad("dashatoledo78@gmail.com", "UPMP");
+
+            button.Text = "Reporte enviado";
+
+        }
+    }
+}
+
